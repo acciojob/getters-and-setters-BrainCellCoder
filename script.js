040c8bf4353eb5e6253f4daaa53b00f5514712f2
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-08-22 19:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    get name() {
+        return this._name;
+    }
+    set age(age) {
+        this._age = age;
+    }
+    get age() {
+        return this._age;
+    }
+}
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
 
-class Student extends Person {}
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+const person = new Person("John", 25);
+console.log(person.name); 
+person.age = 30;
+console.log(person.age);
 
-class Teacher extends Person {}
+const student = new Student("Alice", 22);
+student.study();
 
-// Do not change the code below this line
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); 
+
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
